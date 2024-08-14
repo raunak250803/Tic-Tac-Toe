cc1e47eed627b14ef9b334f54fa95720e2a4cf80
raunak250803 <159190766+raunak250803@users.noreply.github.com>
2024-08-14 23:08:19 +0530

    Update indes.js

--- indes.js
+++ indes.js
@@ -53,11 +53,10 @@ function checkGameOver(){
             }
             else{
                 answer = "0";
-
-                boxes.forEach((box) => {
-                    box.style.pointerEvents = "none";
-                })
             }
+            boxes.forEach((box) => {
+                box.style.pointerEvents = "none";
+            })
             boxes[position[0]].classList.add("win");
             boxes[position[1]].classList.add("win");
             boxes[position[2]].classList.add("win");
